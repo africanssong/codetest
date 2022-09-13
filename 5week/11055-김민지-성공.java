44909eed575d291fc75413afa928e29d498ba2a9
enoma422 <74201624+enoma422@users.noreply.github.com>
2022-09-13 13:36:45 +0900

    Create 11055-김민지-성공.java

---
+++ 5week/11055-김민지-성공.java
@@ -0,0 +1,43 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * #11055
+ * 가장 큰 증가 부분 수열
+ * 2022-09-08
+ */
+public class week5_11055 {
+    /*
+     * 수열 A가 주어졌을 때, 그 수열의 증가 부분 수열 중에서 합이 가장 큰 것을 구하는 프로그램을 작성하시오.
+     * 예를 들어, 수열 A = {1, 100, 2, 50, 60, 3, 5, 6, 7, 8} 인 경우에
+     * 합이 가장 큰 증가 부분 수열은 A = {1, 100, 2, 50, 60, 3, 5, 6, 7, 8} 이고,
+     * 합은 113이다.
+     * */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        int[] A = new int[n];
+        int[] dpArr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            A[i] = in.nextInt();
+        }
+
+        dpArr[0] = A[0];
+
+        for(int i = 1; i < n; i++) {
+            dpArr[i] = A[i];
+            for(int j = 0; j < i; j++) {
+                if(A[i] > A[j]) {
+                    dpArr[i] = Math.max(dpArr[j] + A[i], dpArr[i]);
+                }
+            }
+        }
+
+        System.out.println(Arrays.stream(dpArr).max().getAsInt());
+    }
+}
+
